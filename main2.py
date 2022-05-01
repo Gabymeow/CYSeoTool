adc579fe68bbc0a34c9b9046b9be9ed6d42743fb
Gabymeow <avatarko.994@gmail.com>
2022-05-01 13:13:52 +0300

    Create main2.py
    test class

---
+++ main2.py
@@ -0,0 +1,16 @@
+
+
+
+class Urlactions:
+    def GetUrlName(self, url='site.com'):
+        url = 'https://' + url
+        return url
+
+
+
+""""
+url1 = Urlactions()
+siteName = url1.GetUrlName(input('Введите адрес сайта' +'\n'))
+print(siteName)
+"""
+
